6ffcef949e4ac88506aa0bcf65d65cc43be3d2a1
yanchenyun <yanchenyun@126.com>
2021-07-10 18:22:14 +0800

    勘误

--- 【Flink】第十篇：join 之 regular join/ddl.sql
+++ 【Flink】第十篇：join 之 regular join/ddl.sql
@@ -2,59 +2,59 @@
 --左表, 测试水位线时加上 watermark for op_ts as op_ts
 create table left_upsert(
     appl_seq  string
-	,amount   decimal(16,2)
-	,op_ts    timestamp(3)
-	,primary key(appl_seq) not enforced
+    ,amount   decimal(16,2)
+    ,op_ts    timestamp(3)
+    ,primary key(appl_seq) not enforced
 ) with (
     'connecotr' = 'upsert-kafka'
-	,'topic' = 'left-upsert'
-	,'key.format' = 'json'
-	,'value.format' = 'json'
-	,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'	
-	,'properties.group.id' = 'left-upsert-group'
+    ,'topic' = 'left-upsert'
+    ,'key.format' = 'json'
+    ,'value.format' = 'json'
+    ,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'    
+    ,'properties.group.id' = 'left-upsert-group'
 )
 
 --右表, 测试水位线时加上 watermark for op_ts as op_ts
 create table right_upsert(
     appl_seq  string
-	,amount   decimal(16,2)
-	,op_ts    timestamp(3)
-	,primary key(appl_seq) not enforced
+    ,amount   decimal(16,2)
+    ,op_ts    timestamp(3)
+    ,primary key(appl_seq) not enforced
 ) with (
     'connecotr' = 'upsert-kafka'
-	,'topic' = 'right-upsert'
-	,'key.format' = 'json'
-	,'value.format' = 'json'
-	,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'	
-	,'properties.group.id' = 'right-upsert-group'
+    ,'topic' = 'right-upsert'
+    ,'key.format' = 'json'
+    ,'value.format' = 'json'
+    ,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'    
+    ,'properties.group.id' = 'right-upsert-group'
 )
 
 -- 二、json-kafka
---左表, 测试水位线时加上 watermark for op_ts as op_ts, 测试主键时加上 primary key(appl_seq) not enforced
+--左表, 测试水位线时加上 watermark for op_ts as op_ts
 create table left_json(
     appl_seq  string
-	,amount   decimal(16,2)
-	,op_ts    timestamp(3)
+    ,amount   decimal(16,2)
+    ,op_ts    timestamp(3)
 ) with (
     'connecotr' = 'kafka'
-	,'topic' = 'left-json'
-	,'value.format' = 'json'
-    ,'scan.startup.mode' = 'latest-offset'	
-	,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'	
-	,'properties.group.id' = 'left-json-group'
+    ,'topic' = 'left-json'
+    ,'value.format' = 'json'
+    ,'scan.startup.mode' = 'latest-offset'    
+    ,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'    
+    ,'properties.group.id' = 'left-json-group'
 )
 
---左表, 测试水位线时加上 watermark for op_ts as op_ts, 测试主键时加上 primary key(appl_seq) not enforced
-create table right_upsert(
+--右表, 测试水位线时加上 watermark for op_ts as op_ts
+create table right_json(
     appl_seq  string
-	,amount   decimal(16,2)
-	,op_ts    timestamp(3)
+    ,amount   decimal(16,2)
+    ,op_ts    timestamp(3)
 ) with (
     'connecotr' = 'kafka'
-	,'topic' = 'right-json'
-	,'value.format' = 'json'
-	,'scan.startup.mode' = 'latest-offset'
-	,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'	
-	,'properties.group.id' = 'right-json-group'
+    ,'topic' = 'right-json'
+    ,'value.format' = 'json'
+    ,'scan.startup.mode' = 'latest-offset'
+    ,'properties.bootstrap.servers' = 'xxx:9092,xxx:9092,xxx:9092'    
+    ,'properties.group.id' = 'right-json-group'
 )
 
